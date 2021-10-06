706fb1a9aa9acecfc45ac779725cb1f0ab69876a
Iris Hsu <ishiotsuki@gmail.com>
2021-10-06 02:04:15 -0700

    feat: major refactor for nested parens

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -16,8 +16,8 @@ const input = process.argv.slice(2)[0];
 function calculator(string) {
   // remove extra spaces; reduce +/- operators
   string = string.replace(/\s/g, "").replace("+-", "-").replace("--", "+");
+  console.log("string", string);
   let output;
-  let nums = [];
 
   // operators
   const add = (a, b) => a + b;
@@ -41,42 +41,11 @@ function calculator(string) {
     .split(",")
     .map((el) => {
       if (el == Number(el)) {
-        nums.push(Number(el));
         return Number(el);
       } else {
         return el;
       }
     });
-
-  // check for balanced parens
-  function balancedParens(str) {
-    let openParensStack = [];
-    const openParens = {
-      "(": true,
-    };
-    const closeParens = {
-      ")": "(",
-    };
-
-    for (let i = 0; i < str.length; i++) {
-      let el = str[i];
-      if (openParens[el]) {
-        openParensStack.push(el);
-      }
-      if (closeParens[el]) {
-        if (openParensStack.length === 0) {
-          return false;
-        } else if (
-          closeParens[el] === openParensStack[openParensStack.length - 1]
-        ) {
-          openParensStack.pop();
-        } else {
-          return false;
-        }
-      }
-    }
-    return !openParensStack.length;
-  }
 
   // helper function
   function calculate(array, op, calcFn) {
@@ -87,16 +56,34 @@ function calculator(string) {
     return array.splice(opIdx - 1, 3, output);
   }
 
-  function orderOfOps(arr) {
-    // do all multiplication/division operations first from left to right
-    while (arr.includes("*") || arr.includes("/")) {
-      for (let i = 0; i < arr.length; i++) {
-        let el = arr[i];
-        if (el === "*") calculate(arr, "*", multiply);
-        if (el === "/") calculate(arr, "/", divide);
+  function parseItem(arr, idx) {
+    // check for first index of close parens, then loop backwards to find innermost subArr or first subArr
+    if (arr.includes("(") && arr.includes(")")) {
+      console.log("parens while loop");
+      let end = arr.indexOf(")");
+      for (let j = end; j > idx; j--) {
+        if (arr[j] === "(") {
+          let start = j;
+          let subArr = arr.slice(start + 1, end);
+          console.log("subarr", subArr);
+          let [subtotal] = parseExpression(subArr, 0);
+          console.log("subtotal", subtotal);
+          arr.splice(start - 1, end - start + 3, subtotal);
+          console.log("arr", arr);
+          parseItem(arr, 0);
+        }
       }
+    } else {
+      parseExpression(arr, 0);
     }
-    // then add and subtract from left to right
+    return arr;
+  }
+
+  function parseExpression(arr, idx) {
+    // if no additional parens
+    if (arr.includes("*") || arr.includes("/")) {
+      parseTerm(arr);
+    }
     while (arr.includes("+") || arr.includes("-")) {
       for (let i = 0; i < arr.length; i++) {
         let el = arr[i];
@@ -107,24 +94,22 @@ function calculator(string) {
     return arr;
   }
 
-  // reduce input down to single value
-  while (arr.length > 1) {
-    console.log("initial", arr);
-
-    // operate inside parens first
-    while (arr.includes("(")) {
-      let start = arr.indexOf("(");
-      let end = arr.indexOf(")");
-      let subArr = arr.slice(start + 1, end);
-      let [subtotal] = orderOfOps(subArr);
-      arr.splice(start - 1, end - start + 3, subtotal);
+  function parseTerm(arr) {
+    while (arr.includes("*") || arr.includes("/")) {
+      for (let i = 0; i < arr.length; i++) {
+        let el = arr[i];
+        if (el === "*") calculate(arr, "*", multiply);
+        if (el === "/") calculate(arr, "/", divide);
+      }
     }
-
-    orderOfOps(arr);
+    return arr;
   }
 
-  if (arr.length === 1) {
-    console.log("answer", arr[0]);
+  if (arr.includes("(") && arr.includes(")")) {
+    console.log("parens");
+    parseItem(arr, 0);
+  } else {
+    parseExpression(arr, 0);
   }
 
   return arr[0];
